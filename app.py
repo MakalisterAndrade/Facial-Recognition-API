767687cbf852acb3f82fdac7ce44454716369b38
Makalister Andrade da Silva <2021105100015@aluno.ifro.edu.br>
2024-08-27 20:40:27 -0400

    correção de arquivos temp

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, redirect
 from flask_swagger_ui import get_swaggerui_blueprint
+from controllers.student_controller import student_blueprint
 
 app = Flask(__name__)
 
 # Configuração do Swagger UI
 SWAGGER_URL = '/docs'
-API_URL = '/static/doc.yml'  # Caminho para o arquivo YAML
+API_URL = '/static/doc.yml'
 swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-"""# Redireciona a rota raiz para '/docs'
+# Registro do blueprint
+app.register_blueprint(student_blueprint)
+
+# Redireciona a rota raiz para '/docs'
 @app.route('/')
 def home():
     return redirect('/docs')
-"""
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- models/student_model.py
+++ models/student_model.py
@@ -2,25 +2,29 @@ import os
 import json
 from deepface import DeepFace
 
+# Diretórios necessários
 DATA_DIR = 'data'
 FACE_DIR = 'faces'
+TEMP_DIR = 'temp'
+
+# Crie as pastas se não existirem
+os.makedirs(DATA_DIR, exist_ok=True)
+os.makedirs(FACE_DIR, exist_ok=True)
+os.makedirs(TEMP_DIR, exist_ok=True)
 
 class StudentModel:
     def __init__(self, id_matricula, nome_completo):
         self.id_matricula = id_matricula
         self.nome_completo = nome_completo
         self.student_dir = os.path.join(FACE_DIR, id_matricula)
+        os.makedirs(self.student_dir, exist_ok=True)
         self.data_file = os.path.join(DATA_DIR, f"{id_matricula}.json")
 
     def save_image(self, image_file):
-        if not os.path.exists(self.student_dir):
-            os.makedirs(self.student_dir)
         image_path = os.path.join(self.student_dir, 'face.jpg')
         image_file.save(image_path)
 
     def save_data(self):
-        if not os.path.exists(DATA_DIR):
-            os.makedirs(DATA_DIR)
         data = {
             'id_matricula': self.id_matricula,
             'nome_completo': self.nome_completo,
@@ -31,7 +35,7 @@ class StudentModel:
 
     @staticmethod
     def recognize_student(image_file):
-        temp_image_path = 'temp.jpg'
+        temp_image_path = os.path.join(TEMP_DIR, 'temp.jpg')
         image_file.save(temp_image_path)
 
         for student_file in os.listdir(DATA_DIR):
